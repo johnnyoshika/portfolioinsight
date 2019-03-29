63c7f5889de9ba2bd621be11908920b554918886
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-29 08:38:47 -0700

    Register PortfolioInsightAutofacAssembly, PortfolioInsightDomainAssembly, PortfolioInsightSqlAssembly with Autofac

---
+++ src/PortfolioInsight.Autofac/CommonModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Autofac;
+
+namespace PortfolioInsight
+{
+    public class CommonModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder
+                .RegisterAssemblyOf<PortfolioInsightAutofacAssembly>()
+                .RegisterAssemblyOf<PortfolioInsightDomainAssembly>()
+                .RegisterAssemblyOf<PortfolioInsightSqlAssembly>()
+                .RegisterAssembly(Assembly.GetEntryAssembly());
+        }
+    }
+}
---
+++ src/PortfolioInsight.Domain/PortfolioInsightDomainAssembly.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioInsight
+{
+    public abstract class PortfolioInsightDomainAssembly
+    {
+    }
+}
