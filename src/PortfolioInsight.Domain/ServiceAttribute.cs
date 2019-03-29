e62066dc16510fe1d5b243659346133f0d07bd36
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-29 08:27:14 -0700

    Add AssemblyRegistration for Services

---
+++ src/PortfolioInsight.Domain/ServiceAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioInsight
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ServiceAttribute : Attribute
+    {
+        public bool AsSelf { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SingletonAttribute : ServiceAttribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TransientAttribute : ServiceAttribute
+    {
+    }
+}
