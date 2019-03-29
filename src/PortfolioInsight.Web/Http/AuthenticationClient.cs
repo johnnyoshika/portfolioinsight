9beec24b843f9666965f7b1676fd5d3d4cdf98e7
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-29 09:29:51 -0700

    Add AuthenticationClient

---
+++ src/PortfolioInsight.Web/Http/AuthenticationClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using PortfolioInsight.Users;
+
+namespace PortfolioInsight.Web.Http
+{
+    [Service]
+    public class AuthenticationClient : IAuthenticationClient
+    {
+        public AuthenticationClient(IUserReader userReader)
+        {
+            UserReader = userReader;
+        }
+
+        IUserReader UserReader { get; }
+
+        public async Task<User> AuthenticateAsync(HttpRequest request)
+        {
+            var identity = (ClaimsIdentity)request.HttpContext.User.Identity;
+            if (!identity.IsAuthenticated)
+                throw new UnauthorizedAccessException();
+
+            string token = identity.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value;
+            if (!int.TryParse(token, out int userId))
+                throw new UnauthorizedAccessException();
+
+            return await UserReader.ReadByIdAsync(userId) ?? throw new UnauthorizedAccessException();
+        }
+    }
+}
