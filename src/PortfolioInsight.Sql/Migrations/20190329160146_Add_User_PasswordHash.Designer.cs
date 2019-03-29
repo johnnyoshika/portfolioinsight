46a6a71aef9fd8d51d3bd7b38b68f371f02d3092
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-29 09:02:06 -0700

    Add Add_User_PasswordHash migration

--- src/PortfolioInsight.Sql/Migrations/20190329160146_Add_User_PasswordHash.Designer.cs
+++ src/PortfolioInsight.Sql/Migrations/20190329160146_Add_User_PasswordHash.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PortfolioInsight;
 
 namespace PortfolioInsight.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20190329160146_Add_User_PasswordHash")]
+    partial class Add_User_PasswordHash
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
