1c4dd6e9af1efc83d1ee1005661986b22fd439a3
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-29 08:13:42 -0700

    Add User

---
+++ src/PortfolioInsight.Domain/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioInsight.Users
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastActivityAt { get; set; }
+        public int ActivityCount { get; set; }
+        public DateTime? LastLoginAt { get; set; }
+        public int LoginCount { get; set; }
+    }
+}
