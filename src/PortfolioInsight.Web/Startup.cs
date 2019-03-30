0c255bcd98b7682813a6cd135b56a1fac589fe66
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-29 17:40:48 -0700

    Use lowercase URLs

--- src/PortfolioInsight.Web/Startup.cs
+++ src/PortfolioInsight.Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -36,6 +37,11 @@ namespace PortfolioInsight.Web
                 {
                     o.Cookie.Name = "auth-token";
                 });
+
+            services.Configure<RouteOptions>(options =>
+            {
+                options.LowercaseUrls = true;
+            });
 
             var builder = new ContainerBuilder();
             builder.RegisterSource(new ContravariantRegistrationSource());
