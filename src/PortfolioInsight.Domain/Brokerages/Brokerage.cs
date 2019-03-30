0e7bc077703101d4a2f63b153231b29d5e9165e8
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-30 06:37:44 -0700

    Add Brokerage

---
+++ src/PortfolioInsight.Domain/Brokerages/Brokerage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioInsight.Brokerages
+{
+    public class Brokerage
+    {
+        public static readonly Brokerage Questrade = new Brokerage { Id = 1, Name = "Questrade" };
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
