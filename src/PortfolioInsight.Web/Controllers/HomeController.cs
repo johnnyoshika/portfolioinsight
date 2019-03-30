73fc4f34f2aef2dd90818266a48fefd128611447
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-30 07:12:53 -0700

    Request user authorization from Questrade

--- src/PortfolioInsight.Web/Controllers/HomeController.cs
+++ src/PortfolioInsight.Web/Controllers/HomeController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Security.Authentication;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PortfolioInsight.Authorizations;
+using PortfolioInsight.Configuration;
 using PortfolioInsight.Security;
 using PortfolioInsight.Users;
 using PortfolioInsight.Web.Http;
@@ -18,13 +21,17 @@ namespace PortfolioInsight.Web.Controllers
             IIdentityReader identityReader,
             ISignInClient signInClient,
             ISignOutClient signOutClient,
-            IAuthenticationClient authenticationClient)
+            IAuthenticationClient authenticationClient,
+            IQuestradeSettings questradeSettings,
+            ITokenizer tokenizer)
         {
             UserReader = userReader;
             IdentityReader = identityReader;
             SignInClient = signInClient;
             SignOutClient = signOutClient;
             AuthenticationClient = authenticationClient;
+            QuestradeSettings = questradeSettings;
+            Tokenizer = tokenizer;
         }
 
         IUserReader UserReader { get; }
@@ -32,10 +39,29 @@ namespace PortfolioInsight.Web.Controllers
         ISignInClient SignInClient { get; }
         ISignOutClient SignOutClient { get; }
         IAuthenticationClient AuthenticationClient { get; }
+        IQuestradeSettings QuestradeSettings { get; }
+        ITokenizer Tokenizer { get; }
 
         [Authorize]
         public async Task<IActionResult> Index() =>
             View(await AuthenticationClient.AuthenticateAsync(HttpContext.Request));
+
+        [Authorize]
+        [HttpGet("questrade/request")]
+        public IActionResult QuestradeRequest() =>
+            Redirect($"https://login.questrade.com/oauth2/authorize?client_id={QuestradeSettings.ConsumerKey}&response_type=code&redirect_uri={Request.AbsoluteUrl("/questrade/response").UrlEncode()}");
+
+        [Authorize]
+        [HttpGet("questrade/response")]
+        public async Task<IActionResult> QuestradeResponse(string code)
+        {
+            await Tokenizer.ExchangeAsync(
+                code,
+                await AuthenticationClient.AuthenticateAsync(HttpContext.Request),
+                Request.AbsoluteUrl("/questrade/response").UrlEncode());
+
+            return Redirect("/");
+        }
 
         [HttpGet("account/login")]
         public IActionResult Login() => View();
