75947a144ce21ee55485b913e2d71f4ee8602237
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-29 23:48:10 -0700

    Add WebFormat UrlEncode/HtmlEncode extensions

---
+++ src/PortfolioInsight.Domain/Utilities/WebFormat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace System
+{
+    public static class WebFormat
+    {
+        public static string HtmlEncode(this string s) =>
+            WebUtility.HtmlEncode(s);
+
+        public static string UrlEncode(this string s) =>
+            WebUtility.UrlEncode(s);
+
+        public static string HtmlEncodeExcept(this string s, string tag) =>
+            s.Replace($"<{tag}>", $"*$*{tag}*$*")
+            .Replace($"</{tag}>", $"*$*/{tag}*$*")
+            .HtmlEncode()
+            .Replace($"*$*{tag}*$*", $"<{tag}>")
+            .Replace($"*$*/{tag}*$*", $"</{tag}>");
+    }
+}
