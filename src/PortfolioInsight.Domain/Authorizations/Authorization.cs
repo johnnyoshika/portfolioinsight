012eb752eb7398501f69a78899339e21efc19616
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-29 23:46:58 -0700

    Add Authorization

---
+++ src/PortfolioInsight.Domain/Authorizations/Authorization.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PortfolioInsight.Brokerages;
+using PortfolioInsight.Users;
+
+namespace PortfolioInsight.Authorizations
+{
+    public class Authorization
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Brokerage Brokerage { get; set; }
+        public string BrokerageUserId { get; set; }
+        public string RefreshToken { get; set; }
+        public bool IsValid { get; set; }
+    }
+}
