12caf875de5170cd0841a54132edf364e00136df
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-31 08:19:16 -0700

    Drop_Authorization_IsValid migration

---
+++ src/PortfolioInsight.Sql/Migrations/20190331151843_Drop_Authorization_IsValid.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortfolioInsight.Migrations
+{
+    public partial class Drop_Authorization_IsValid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsValid",
+                table: "Authorizations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsValid",
+                table: "Authorizations",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
