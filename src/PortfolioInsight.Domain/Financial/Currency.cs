1416a25c563f9d8752c230a77635bd46e2ac3539
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-31 08:50:16 -0700

    Add Currency

---
+++ src/PortfolioInsight.Domain/Financial/Currency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioInsight.Financial
+{
+    public class Currency : ValueObject<Currency>
+    {
+        public static readonly Currency Unknown = new Currency(null);
+
+        public Currency(string code)
+        {
+            Code = code.ToUpper();
+        }
+
+        public string Code { get; }
+
+        protected override IEnumerable<object> EqualityCheckAttributes =>
+            new object[] { Code };
+    }
+}
