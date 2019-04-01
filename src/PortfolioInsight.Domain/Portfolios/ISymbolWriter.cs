91c49852aa15d7f6ecb4880305a935c0f614851a
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-31 23:30:38 -0700

    Add Portfolio interfaces

---
+++ src/PortfolioInsight.Domain/Portfolios/ISymbolWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioInsight.Portfolios
+{
+    public interface ISymbolWriter
+    {
+        Task<Symbol> WriteAsync(int brokerageId, string referenceId, string name);
+    }
+}
