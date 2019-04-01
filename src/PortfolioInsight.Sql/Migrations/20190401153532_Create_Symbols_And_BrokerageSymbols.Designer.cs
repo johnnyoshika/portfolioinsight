a47216d6e24e1a869c23a371acb5d2db8fe87982
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-01 08:36:25 -0700

    Create_Symbols_And_BrokerageSymbols migration

--- src/PortfolioInsight.Sql/Migrations/20190401153532_Create_Symbols_And_BrokerageSymbols.Designer.cs
+++ src/PortfolioInsight.Sql/Migrations/20190401153532_Create_Symbols_And_BrokerageSymbols.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PortfolioInsight;
 
 namespace PortfolioInsight.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20190401153532_Create_Symbols_And_BrokerageSymbols")]
+    partial class Create_Symbols_And_BrokerageSymbols
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
