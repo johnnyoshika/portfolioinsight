a71ae4b0fb8251b899f903bdded7ac7baadedfb9
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-02 08:21:23 -0700

    Balances_CurrencyCode_ForeignKey_Constraint migration

--- src/PortfolioInsight.Sql/Migrations/20190402152017_Balances_CurrencyCode_ForeignKey_Constraint.Designer.cs
+++ src/PortfolioInsight.Sql/Migrations/20190402152017_Balances_CurrencyCode_ForeignKey_Constraint.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PortfolioInsight;
 
 namespace PortfolioInsight.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20190402152017_Balances_CurrencyCode_ForeignKey_Constraint")]
+    partial class Balances_CurrencyCode_ForeignKey_Constraint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/PortfolioInsight.Sql/Migrations/20190402152017_Balances_CurrencyCode_ForeignKey_Constraint.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortfolioInsight.Migrations
+{
+    public partial class Balances_CurrencyCode_ForeignKey_Constraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Balances_CurrencyCode",
+                table: "Balances",
+                column: "CurrencyCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Balances_Type_CurrencyCode_AccountId",
+                table: "Balances",
+                columns: new[] { "Type", "CurrencyCode", "AccountId" },
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Balances_Currencies_CurrencyCode",
+                table: "Balances",
+                column: "CurrencyCode",
+                principalTable: "Currencies",
+                principalColumn: "Code",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Balances_Currencies_CurrencyCode",
+                table: "Balances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Balances_CurrencyCode",
+                table: "Balances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Balances_Type_CurrencyCode_AccountId",
+                table: "Balances");
+        }
+    }
+}
