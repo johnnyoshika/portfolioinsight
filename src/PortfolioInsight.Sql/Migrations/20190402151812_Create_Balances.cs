87a6b426ac3d14254779c4c8c00ee2c5cd23935e
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-02 08:18:32 -0700

    Create_Balances migration

---
+++ src/PortfolioInsight.Sql/Migrations/20190402151812_Create_Balances.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortfolioInsight.Migrations
+{
+    public partial class Create_Balances : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Balances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Value = table.Column<decimal>(type: "money", nullable: false),
+                    Type = table.Column<string>(maxLength: 10, nullable: false),
+                    CurrencyCode = table.Column<string>(maxLength: 3, nullable: false),
+                    AccountId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Balances", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Balances_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Balances_AccountId",
+                table: "Balances",
+                column: "AccountId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Balances");
+        }
+    }
+}
