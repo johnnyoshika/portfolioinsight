a50fa28e602d359ae7787533f0a8669f81092a9d
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-03 06:59:33 -0700

    Add Questrade Enumerations

---
+++ src/PortfolioInsight.Questrade/Http/Enumerations.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioInsight.Http
+{
+    enum QuestradeAccountType
+    {
+        Cash,
+        Margin,
+        TFSA,
+        RRSP,
+        SRRSP,
+        LRRSP,
+        LIRA,
+        LIF,
+        RIF,
+        SRIF,
+        LRIF,
+        RRIF,
+        PRIF,
+        RESP,
+        FRESP
+    }
+
+    enum QuestradeCurrency
+    {
+        USD,
+        CAD
+    }
+
+    enum QuestradeListingExchange
+    {
+        TSX,
+        TSXV,
+        CNSX,
+        MX,
+        NASDAQ,
+        NYSE,
+        NYSEAM,
+        ARCA,
+        OPRA,
+        PinkSheets,
+        OTCBB
+    }
+
+    enum QuestradeSecurityType
+    {
+        Stock,
+        Option,
+        Bond,
+        Right,
+        Gold,
+        MutualFund,
+        Index
+    }
+}
