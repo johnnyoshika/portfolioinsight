942958872fae6460f272bf9713b026db8e48706f
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-03 08:28:23 -0700

    Add ISymbolReader

---
+++ src/PortfolioInsight.Domain/Portfolios/ISymbolReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioInsight.Portfolios
+{
+    public interface ISymbolReader
+    {
+        Task<Symbol> ReadByIdAsync(int id);
+        Task<Symbol> ReadByBrokerageReferenceAsync(int brokerageId, string referenceId);
+        Task<Symbol> ReadByBrokerageNameAsync(int brokerageId, string name);
+    }
+}
