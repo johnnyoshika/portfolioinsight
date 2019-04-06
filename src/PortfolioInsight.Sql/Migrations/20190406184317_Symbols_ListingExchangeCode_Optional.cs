08559cf7c38eed793afee95a41697c7de81cdc3b
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-06 11:47:55 -0700

    Symbols_ListingExchangeCode_Optional migration

---
+++ src/PortfolioInsight.Sql/Migrations/20190406184317_Symbols_ListingExchangeCode_Optional.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortfolioInsight.Migrations
+{
+    public partial class Symbols_ListingExchangeCode_Optional : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Symbols_Name_ListingExchangeCode",
+                table: "Symbols");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ListingExchangeCode",
+                table: "Symbols",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 10);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Symbols_Name_ListingExchangeCode",
+                table: "Symbols",
+                columns: new[] { "Name", "ListingExchangeCode" },
+                unique: true,
+                filter: "[ListingExchangeCode] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Symbols_Name_ListingExchangeCode",
+                table: "Symbols");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ListingExchangeCode",
+                table: "Symbols",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Symbols_Name_ListingExchangeCode",
+                table: "Symbols",
+                columns: new[] { "Name", "ListingExchangeCode" },
+                unique: true);
+        }
+    }
+}
