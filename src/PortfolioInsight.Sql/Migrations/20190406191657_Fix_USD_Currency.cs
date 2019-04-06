e710edb80f10aa04a07542e35f768dc73d64fdd9
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-06 12:17:57 -0700

    Add Fix_USD_Currency migration

---
+++ src/PortfolioInsight.Sql/Migrations/20190406191657_Fix_USD_Currency.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortfolioInsight.Migrations
+{
+    public partial class Fix_USD_Currency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Code",
+                keyValue: "USA");
+
+            migrationBuilder.InsertData(
+                table: "Currencies",
+                column: "Code",
+                value: "USD");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Code",
+                keyValue: "USD");
+
+            migrationBuilder.InsertData(
+                table: "Currencies",
+                column: "Code",
+                value: "USA");
+        }
+    }
+}
