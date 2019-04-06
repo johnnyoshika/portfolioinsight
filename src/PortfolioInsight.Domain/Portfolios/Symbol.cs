c55164a9fd0c7716e719d2f38265e6dfd4f297f8
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-06 09:13:44 -0700

    Add Symbol.ListingExchange

--- src/PortfolioInsight.Domain/Portfolios/Symbol.cs
+++ src/PortfolioInsight.Domain/Portfolios/Symbol.cs
@@ -7,15 +7,17 @@ namespace PortfolioInsight.Portfolios
 {
     public class Symbol
     {
-        public Symbol(int id, string name, Currency currency)
+        public Symbol(int id, string name, Currency currency, string listingExchange)
         {
             Id = id;
             Name = name;
             Currency = currency;
+            ListingExchange = listingExchange;
         }
 
         public int Id { get; }
         public string Name { get; }
         public Currency Currency { get; }
+        public string ListingExchange { get; }
     }
 }
