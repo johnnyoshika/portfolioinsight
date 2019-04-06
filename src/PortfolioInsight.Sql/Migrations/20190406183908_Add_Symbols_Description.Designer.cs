cc263ff287a483c038c4483ce601b7442869035e
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-06 11:39:34 -0700

    Add_Symbols_Description migration

--- src/PortfolioInsight.Sql/Migrations/20190406183908_Add_Symbols_Description.Designer.cs
+++ src/PortfolioInsight.Sql/Migrations/20190406183908_Add_Symbols_Description.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PortfolioInsight;
 
 namespace PortfolioInsight.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20190406183908_Add_Symbols_Description")]
+    partial class Add_Symbols_Description
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/PortfolioInsight.Sql/Migrations/20190406183908_Add_Symbols_Description.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortfolioInsight.Migrations
+{
+    public partial class Add_Symbols_Description : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Symbols",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Symbols");
+        }
+    }
+}
