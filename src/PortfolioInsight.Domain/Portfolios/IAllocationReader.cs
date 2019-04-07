38a85efa2610297df13c877fe84f303485692065
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-07 13:34:54 -0700

    Add Allocation Reader / Writer interfaces

---
+++ src/PortfolioInsight.Domain/Portfolios/IAllocationReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioInsight.Portfolios
+{
+    public interface IAllocationReader
+    {
+        Task<List<Allocation>> ReadByUserIdAsync(int userId);
+    }
+}
---
+++ src/PortfolioInsight.Domain/Portfolios/IAllocationWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioInsight.Portfolios
+{
+    public interface IAllocationWriter
+    {
+        Task WriteAsync(int userId, Allocation allocation);
+    }
+}
