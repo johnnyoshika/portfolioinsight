5ef9b81530eeefb98c2471b8d1d3e28fd3133857
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-07 21:26:36 -0700

    Implement CurrencyReader

---
+++ src/PortfolioInsight.Sql/Entities/CurrencyEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PortfolioInsight.Financial;
+
+namespace PortfolioInsight
+{
+    public partial class CurrencyEntity
+    {
+        public Currency ToModel() =>
+            new Currency(Code, (Rate)Rate);
+    }
+}
---
+++ src/PortfolioInsight.Sql/Financial/CurrencyReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PortfolioInsight.Financial
+{
+    [Service]
+    public class CurrencyReader : ICurrencyReader
+    {
+        public CurrencyReader(Func<Context> context)
+        {
+            Context = context;
+        }
+
+        Func<Context> Context { get; }
+
+        public async Task<Currency> ReadByCodeAsync(string code)
+        {
+            using (var context = Context())
+                return await context
+                    .Currencies
+                    .Select(c => c.ToModel())
+                    .FirstAsync(c => c.Code == code);
+        }
+    }
+}
