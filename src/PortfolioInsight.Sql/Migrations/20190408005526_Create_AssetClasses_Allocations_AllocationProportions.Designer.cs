4221bc015f7f2c23cabb978a53852a077462c2bd
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-07 17:57:05 -0700

    Create_AssetClasses_Allocations_AllocationProportions migration

--- src/PortfolioInsight.Sql/Migrations/20190408005526_Create_AssetClasses_Allocations_AllocationProportions.Designer.cs
+++ src/PortfolioInsight.Sql/Migrations/20190408005526_Create_AssetClasses_Allocations_AllocationProportions.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PortfolioInsight;
 
 namespace PortfolioInsight.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20190408005526_Create_AssetClasses_Allocations_AllocationProportions")]
+    partial class Create_AssetClasses_Allocations_AllocationProportions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/PortfolioInsight.Sql/Migrations/20190408005526_Create_AssetClasses_Allocations_AllocationProportions.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortfolioInsight.Migrations
+{
+    public partial class Create_AssetClasses_Allocations_AllocationProportions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Allocations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(nullable: false),
+                    SymbolId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Allocations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Allocations_Symbols_SymbolId",
+                        column: x => x.SymbolId,
+                        principalTable: "Symbols",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Allocations_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssetClasses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Target = table.Column<decimal>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssetClasses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssetClasses_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AllocationProportions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AllocationId = table.Column<int>(nullable: false),
+                    AssetClassId = table.Column<int>(nullable: false),
+                    Rate = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AllocationProportions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AllocationProportions_Allocations_AllocationId",
+                        column: x => x.AllocationId,
+                        principalTable: "Allocations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AllocationProportions_AssetClasses_AssetClassId",
+                        column: x => x.AssetClassId,
+                        principalTable: "AssetClasses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AllocationProportions_AssetClassId",
+                table: "AllocationProportions",
+                column: "AssetClassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AllocationProportions_AllocationId_AssetClassId",
+                table: "AllocationProportions",
+                columns: new[] { "AllocationId", "AssetClassId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Allocations_SymbolId",
+                table: "Allocations",
+                column: "SymbolId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Allocations_UserId_SymbolId",
+                table: "Allocations",
+                columns: new[] { "UserId", "SymbolId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetClasses_UserId",
+                table: "AssetClasses",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetClasses_Name_UserId",
+                table: "AssetClasses",
+                columns: new[] { "Name", "UserId" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AllocationProportions");
+
+            migrationBuilder.DropTable(
+                name: "Allocations");
+
+            migrationBuilder.DropTable(
+                name: "AssetClasses");
+        }
+    }
+}
