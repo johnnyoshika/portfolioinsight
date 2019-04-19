3d16a4565f09f6eb1442f0157c3ec1928e2d2a2c
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-19 01:07:29 -0700

    Increase_Precision_On_CurrencyRate_And_AssetClassTarget migration

--- src/PortfolioInsight.Sql/Migrations/20190419080558_Increase_Precision_On_CurrencyRate_And_AssetClassTarget.Designer.cs
+++ src/PortfolioInsight.Sql/Migrations/20190419080558_Increase_Precision_On_CurrencyRate_And_AssetClassTarget.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PortfolioInsight;
 
 namespace PortfolioInsight.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20190419080558_Increase_Precision_On_CurrencyRate_And_AssetClassTarget")]
+    partial class Increase_Precision_On_CurrencyRate_And_AssetClassTarget
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
