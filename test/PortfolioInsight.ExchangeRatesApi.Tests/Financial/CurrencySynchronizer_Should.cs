3c294678b8456c74a8495b2a0ee594a708ea4f2c
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-19 00:38:10 -0700

    Add CurrencySynchronizer_Should unit test

---
+++ test/PortfolioInsight.ExchangeRatesApi.Tests/Financial/CurrencySynchronizer_Should.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using PortfolioInsight.Financial;
+using Xunit;
+
+namespace PortfolioInsight.ExchangeRatesApi.Tests.Financial
+{
+    public class CurrencySynchronizer_Should
+    {
+        [Fact]
+        public async Task Synchronize()
+        {
+            var currencyReader = new Mock<ICurrencyReader>();
+            currencyReader.Setup(_ => _.ReadAllAsync())
+                .ReturnsAsync(new List<Currency>
+                {
+                    new Currency("CAD", Rate.Full, DateTime.UtcNow.Date),
+                    new Currency("USD", Rate.Full, DateTime.UtcNow.Date)
+                });
+
+            var currencyWriter = new Mock<ICurrencyWriter>();
+            
+            var synchronizer = new CurrencySynchronizer(currencyReader.Object, currencyWriter.Object);
+            await synchronizer.SyncAsync();
+
+            currencyWriter.Verify(_ =>
+                _.WriteAsync(It.IsAny<Currency>()),
+                Times.Exactly(2));
+
+            currencyWriter.Verify(_ =>
+                _.WriteAsync(It.Is<Currency>(c => c.Code == "CAD")),
+                Times.Once);
+
+            currencyWriter.Verify(_ =>
+                _.WriteAsync(It.Is<Currency>(c => c.Code == "USD" && c.Rate == Rate.Full)),
+                Times.Once);
+        }
+    }
+}
