339599ec3d4dfc77c96439b5e17f0f677a29d17c
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-19 00:45:13 -0700

    Implement CurrencyWriter

---
+++ src/PortfolioInsight.Sql/Financial/CurrencyWriter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PortfolioInsight.Financial
+{
+    [Service]
+    public class CurrencyWriter : ICurrencyWriter
+    {
+        public CurrencyWriter(Func<Context> context)
+        {
+            Context = context;
+        }
+
+        Func<Context> Context { get; }
+
+        public async Task WriteAsync(Currency currency)
+        {
+            using (var context = Context())
+            {
+                var eCurrency = await context
+                    .Currencies
+                    .Where(c => c.Code == currency.Code)
+                    .FirstAsync();
+
+                eCurrency.Rate = currency.Rate;
+                eCurrency.AsOf = currency.AsOf;
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
