b14e9c3c3dac067662f15cc088146c9b4dc275dc
Johnny Oshika <johnnyoshika@gmail.com>
2019-04-18 23:57:30 -0700

    Add ICurrencySynchronizer and ICurrencyWriter

---
+++ src/PortfolioInsight.Domain/Financial/ICurrencyWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioInsight.Financial
+{
+    public interface ICurrencyWriter
+    {
+        Task WriteAsync(Currency currency);
+    }
+}
