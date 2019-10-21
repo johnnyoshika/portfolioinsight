e07ff79e610b8b9ec1bc3d233e9dbe9f24f5ce68
Johnny Oshika <johnnyoshika@gmail.com>
2019-10-21 14:30:30 -0700

    Migrate Program and Startup to .Net Core 3.0

--- src/PortfolioInsight.Web/Startup.cs
+++ src/PortfolioInsight.Web/Startup.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Autofac.Features.Variance;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using PortfolioInsight.Configuration;
 
 namespace PortfolioInsight.Web
@@ -29,9 +26,9 @@ namespace PortfolioInsight.Web
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public IServiceProvider ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllersWithViews();
 
             services.Configure<ConfigSettings>(Configuration);
 
@@ -45,27 +42,28 @@ namespace PortfolioInsight.Web
             {
                 options.LowercaseUrls = true;
             });
+        }
 
-            var builder = new ContainerBuilder();
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
             builder.RegisterSource(new ContravariantRegistrationSource());
             builder.RegisterModule(new CommonModule(ConnectionString));
             builder.RegisterModule(new WebModule());
-            builder.Populate(services);
             ApplicationContainer = builder.Build();
-            return new AutofacServiceProvider(ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime applicationLifetime)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime applicationLifetime)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseRouting();
             app.UseAuthentication();
-
-            app.UseMvcWithDefaultRoute();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
 
             applicationLifetime.ApplicationStopped.Register(() => ApplicationContainer.Dispose());
         }
