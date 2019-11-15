9eed35ec4b813e5f84a123e1d44d1bc59f4d22ee
Johnny Oshika <johnnyoshika@gmail.com>
2019-11-15 10:05:14 -0800

    Add Rename_Authorization_To_Connection migration

--- src/PortfolioInsight.Sql/Migrations/20191115175628_Rename_Authorizations_To_Connections.Designer.cs
+++ src/PortfolioInsight.Sql/Migrations/20191115175628_Rename_Authorizations_To_Connections.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PortfolioInsight;
 
 namespace PortfolioInsight.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20191115175628_Rename_Authorizations_To_Connections")]
+    partial class Rename_Authorizations_To_Connections
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
