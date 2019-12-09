d1c75890527f0ed03023ae1f3f61a00ce3fa66ca
Johnny Oshika <johnnyoshika@gmail.com>
2019-12-08 23:01:18 -0800

    Add Portfolio and PortfolioReader

---
+++ src/PortfolioInsight.Sql/Entities/PortfolioEntity.cs
@@ -0,0 +1,13 @@
+﻿using PortfolioInsight.Portfolios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioInsight
+{
+    public partial class PortfolioEntity
+    {
+        public Portfolio ToModel() =>
+            new Portfolio(Id, Name);
+    }
+}
