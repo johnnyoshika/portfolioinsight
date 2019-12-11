e81fd0fa4758fa04876e04b05a399bdb466724a7
Johnny Oshika <johnnyoshika@gmail.com>
2019-12-11 12:15:52 -0800

    Add Exclude and ExcludeReader

---
+++ src/PortfolioInsight.Domain/Portfolios/IExcludeReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioInsight.Portfolios
+{
+    public interface IExcludeReader
+    {
+        Task<Exclude> ReadByPortfolioIdAsync(int portfolioId);
+    }
+}
