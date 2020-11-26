0983d97c099fadbfe83cc01fd50e0241a971aab7
Johnny Oshika <johnnyoshika@gmail.com>
2020-11-26 10:28:02 -0800

    Add migration after EntityFrameworkCore upgrade to version 5

---
+++ src/PortfolioInsight.Sql/Migrations/20201126182720_Entity_Framework_Core_Upgrade.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortfolioInsight.Migrations
+{
+    public partial class Entity_Framework_Core_Upgrade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
