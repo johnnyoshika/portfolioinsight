e5cbb84a8f24080d9b5359139c2f51ecb314a28d
Johnny Oshika <johnnyoshika@gmail.com>
2021-01-12 10:29:14 -0800

    Add Make_Symbol_Name_Unique migration

---
+++ src/PortfolioInsight.Sql/Migrations/20210112182825_Make_Symbol_Name_Unique.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortfolioInsight.Migrations
+{
+    public partial class Make_Symbol_Name_Unique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Symbols_Name_ListingExchangeCode",
+                table: "Symbols");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Symbols_Name",
+                table: "Symbols",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Symbols_Name",
+                table: "Symbols");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Symbols_Name_ListingExchangeCode",
+                table: "Symbols",
+                columns: new[] { "Name", "ListingExchangeCode" },
+                unique: true,
+                filter: "[ListingExchangeCode] IS NOT NULL");
+        }
+    }
+}
