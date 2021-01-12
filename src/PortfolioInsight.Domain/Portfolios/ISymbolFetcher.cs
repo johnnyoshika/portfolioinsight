45d91b4f6bd7680f0a2bf76e0946e6f57b8fab20
Johnny Oshika <johnnyoshika@gmail.com>
2021-01-12 10:43:19 -0800

    Add SymbolFetcher

---
+++ src/PortfolioInsight.Domain/Portfolios/ISymbolFetcher.cs
@@ -0,0 +1,14 @@
+﻿using PortfolioInsight.Connections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioInsight.Portfolios
+{
+    public interface ISymbolFetcher
+    {
+        Task<NewSymbol> FetchByNameAsync(string name, Connection connection);
+    }
+}
