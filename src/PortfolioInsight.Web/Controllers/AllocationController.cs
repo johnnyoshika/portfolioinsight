a087dcf333115925d61ad99d9639d6b07062f7d0
Johnny Oshika <johnnyoshika@gmail.com>
2021-06-03 13:46:45 -0700

    Reduce bond allocation and increase SCV and Canada

--- src/PortfolioInsight.Web/Controllers/AllocationController.cs
+++ src/PortfolioInsight.Web/Controllers/AllocationController.cs
@@ -75,15 +75,15 @@ namespace PortfolioInsight.Web.Controllers
 
             var allocations = new Allocation[]
             {
-                new Allocation(await SymbolReader.ReadByNameAsync("ZDB.TO"              ), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "BOND", (Rate)0.30m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("ZAG.TO"              ), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "BOND", (Rate)0.30m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("XIC.TO"              ), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CA", (Rate)0.21m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("ZDB.TO"              ), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "BOND", (Rate)0.275m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("ZAG.TO"              ), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "BOND", (Rate)0.275m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("XIC.TO"              ), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CA", (Rate)0.217m), Rate.Full)}),
                 new Allocation(await SymbolReader.ReadByNameAsync("XUU.TO"              ), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.21m), Rate.Full)}),
                 new Allocation(await SymbolReader.ReadByNameAsync("VUN.TO",   connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.21m), Rate.Full)}),
                 new Allocation(await SymbolReader.ReadByNameAsync("ITOT"                ), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.21m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("AVUV",     connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US SV", (Rate)0.07m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("AVUV",     connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US SCV", (Rate)0.08m), Rate.Full)}),
                 new Allocation(await SymbolReader.ReadByNameAsync("XEF.TO"              ), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "INTL", (Rate)0.112m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("AVDV",     connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "INTL SV", (Rate)0.042m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("AVDV",     connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "INTL SCV", (Rate)0.05m), Rate.Full)}),
                 new Allocation(await SymbolReader.ReadByNameAsync("XEC.TO"              ), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "EM", (Rate)0.056m), Rate.Full)}),
                 new Allocation(await SymbolReader.ReadByNameAsync("DLR.TO"              ), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CASH", Rate.Zero), Rate.Full)}),
                 new Allocation(await SymbolReader.ReadByNameAsync("DLR.U.TO", connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CASH", Rate.Zero), Rate.Full)}),
