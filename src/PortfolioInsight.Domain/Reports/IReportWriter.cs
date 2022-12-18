899e352d92e5f30089cdbfe9b6f876fb593f388f
Johnny Oshika <johnnyoshika@gmail.com>
2022-12-17 18:58:34 -0800

    Add ReportReader and ReportWriter

---
+++ src/PortfolioInsight.Domain/Reports/IReportWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioInsight.Reports
+{
+    public interface IReportWriter
+    {
+        Task WriteAsync(int portfolioId, DateTime date, Report report);
+    }
+}
---
+++ src/PortfolioInsight.Sql/Reports/ReportWriter.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioInsight.Reports
+{
+    [Service]
+    public class ReportWriter : IReportWriter
+    {
+        public ReportWriter(Func<Context> context)
+        {
+            Context = context;
+        }
+
+        Func<Context> Context { get; }
+
+        public async Task WriteAsync(int portfolioId, DateTime date, Report report)
+        {
+            using (var context = Context())
+            {
+                var eReport = await context
+                    .Reports
+                    .Where(r => r.PortfolioId == portfolioId && r.Date == date)
+                    .FirstOrDefaultAsync();
+
+                if (eReport == null)
+                {
+                    eReport = new ReportEntity
+                    {
+                        PortfolioId = portfolioId,
+                        Date = date,
+                    };
+                    context.Reports.Add(eReport);
+                }
+
+                eReport.Assign(DateTime.UtcNow, report);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
