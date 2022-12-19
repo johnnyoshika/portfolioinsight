1bdcfd9dbef5872eb48eadbe56ce1bf7d98c989f
Johnny Oshika <johnnyoshika@gmail.com>
2022-12-18 22:07:03 -0800

    Pass AccessToken to SymbolReader.ReadByNameAsync()

--- src/PortfolioInsight.Web/Controllers/AllocationController.cs
+++ src/PortfolioInsight.Web/Controllers/AllocationController.cs
@@ -15,14 +15,14 @@ namespace PortfolioInsight.Web.Controllers
     public class AllocationController : Controller
     {
         public AllocationController(
-            IAuthenticationClient authenticationClient,
+            ITokenizer tokenizer,
             IPortfolioReader portfolioReader,
             IConnectionReader connectionReader,
             ISymbolReader symbolReader,
             IAllocationWriter allocationWriter,
             IAssetClassWriter assetClassWriter)
         {
-            AuthenticationClient = authenticationClient;
+            Tokenizer = tokenizer;
             PortfolioReader = portfolioReader;
             ConnectionReader = connectionReader;
             SymbolReader = symbolReader;
@@ -30,7 +30,7 @@ namespace PortfolioInsight.Web.Controllers
             AssetClassWriter = assetClassWriter;
         }
 
-        IAuthenticationClient AuthenticationClient { get; }
+        ITokenizer Tokenizer { get; }
         IPortfolioReader PortfolioReader { get; }
         IConnectionReader ConnectionReader { get; }
         ISymbolReader SymbolReader { get; }
@@ -41,54 +41,56 @@ namespace PortfolioInsight.Web.Controllers
         [HttpPut]
         public async Task<IActionResult> Index()
         {
-            await Portfolio1();
-            await Portfolio5();
+            var connection = await ConnectionReader.ReadByIdAsync(1);
+            // Using 'accessToken' b/c we want SymbolReader.ReadByNameAsync to create a new symbol if one doesn't exist
+            var accessToken = await Tokenizer.RefreshAsync(connection);
+            await Portfolio1(accessToken);
+            await Portfolio5(accessToken);
             return NoContent();
         }
 
-        async Task Portfolio1()
+        async Task Portfolio1(AccessToken accessToken)
         {
             int portfolioId = 1;
-            var connection = await ConnectionReader.ReadByIdAsync(1); // Using 'connection' b/c we want SymbolReader.ReadByNameAsync to create a new symbol if one doesn't exist
 
             var allocations = new Allocation[]
             {
-                new Allocation(await SymbolReader.ReadByNameAsync("XEF.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "INTL", (Rate)0.16m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("XEC.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "EM", (Rate)0.065m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("XIC.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CA", (Rate)0.225m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("XUU.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.25m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("VUN.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.25m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("ITOT"    , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.25m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("ZDB.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "BOND", (Rate)0.30m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("ZAG.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "BOND", (Rate)0.30m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("DLR.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CASH", Rate.Zero), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("DLR.U.TO", connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CASH", Rate.Zero), Rate.Full)})
+                new Allocation(await SymbolReader.ReadByNameAsync("XEF.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "INTL", (Rate)0.16m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("XEC.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "EM", (Rate)0.065m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("XIC.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CA", (Rate)0.225m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("XUU.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.25m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("VUN.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.25m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("ITOT"    , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.25m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("ZDB.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "BOND", (Rate)0.30m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("ZAG.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "BOND", (Rate)0.30m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("DLR.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CASH", Rate.Zero), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("DLR.U.TO", accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CASH", Rate.Zero), Rate.Full)})
             };
 
             foreach (var allocation in allocations)
                 await AllocationWriter.WriteAsync(portfolioId, allocation);
         }
 
-        async Task Portfolio5()
+        async Task Portfolio5(AccessToken accessToken)
         {
             int portfolioId = 5;
             var connection = await ConnectionReader.ReadByIdAsync(1); // Using 'connection' b/c we want SymbolReader.ReadByNameAsync to create a new symbol if one doesn't exist
 
             var allocations = new Allocation[]
             {
-                new Allocation(await SymbolReader.ReadByNameAsync("ZDB.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "BOND", (Rate)0.275m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("ZAG.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "BOND", (Rate)0.275m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("XIC.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CA", (Rate)0.217m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("XUU.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.21m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("VUN.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.21m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("ITOT"    , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.21m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("AVUV"    , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US SCV", (Rate)0.08m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("XEF.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "INTL", (Rate)0.112m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("AVDV"    , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "INTL SCV", (Rate)0.05m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("XEC.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "EM", (Rate)0.056m), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("DLR.TO"  , connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CASH", Rate.Zero), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("DLR.U.TO", connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CASH", Rate.Zero), Rate.Full)}),
-                new Allocation(await SymbolReader.ReadByNameAsync("DLR.U.TO", connection), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CASH", Rate.Zero), Rate.Full)})
+                new Allocation(await SymbolReader.ReadByNameAsync("ZDB.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "BOND", (Rate)0.275m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("ZAG.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "BOND", (Rate)0.275m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("XIC.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CA", (Rate)0.217m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("XUU.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.21m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("VUN.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.21m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("ITOT"    , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US", (Rate)0.21m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("AVUV"    , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "US SCV", (Rate)0.08m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("XEF.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "INTL", (Rate)0.112m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("AVDV"    , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "INTL SCV", (Rate)0.05m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("XEC.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "EM", (Rate)0.056m), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("DLR.TO"  , accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CASH", Rate.Zero), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("DLR.U.TO", accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CASH", Rate.Zero), Rate.Full)}),
+                new Allocation(await SymbolReader.ReadByNameAsync("DLR.U.TO", accessToken), new[]{ new AllocationProportion(await AssetClassWriter.WriteAsync(portfolioId, "CASH", Rate.Zero), Rate.Full)})
             };
 
             foreach (var allocation in allocations)
